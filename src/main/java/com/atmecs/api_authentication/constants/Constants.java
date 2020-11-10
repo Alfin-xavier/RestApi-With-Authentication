9b9abdf7df9b889dcfbb0d70eb7264e96549531f
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-11-10 17:20:36 +0530

    Updated

--- src/main/java/com/atmecs/api_authentication/constants/Constants.java
+++ src/main/java/com/atmecs/api_authentication/constants/Constants.java
@@ -2,6 +2,6 @@ package com.atmecs.api_authentication.constants;
 
 public class Constants {
 
-	public static final String JSON_DATA_FILE = null;
+	public static final String JSON_DATA_FILE = "./src/main/resources/datas/user.json";
 
 }
